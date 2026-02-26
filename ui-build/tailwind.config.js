5bac5cdbb9fb5ecfc52f0cb79f4e388bb13c6530
John White <john@johnpwhite.com>
2026-02-27 10:19:52 +1100

    Critical CSS scoping fix (v2026.02.27.08)

--- ui-build/tailwind.config.js
+++ ui-build/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	darkMode: ["class"],
-	important: '.copilot-ui',
+	important: '.gemini-cli-ui',
 	content: [
 		"./index.html",
 		"./src/**/*.{js,ts,jsx,tsx}",
